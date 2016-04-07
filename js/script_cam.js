fe77323f521e0514951dd417cbbf3e057db25621
Camille-D <camille.dagnaud@gmail.com>
2016-04-07 09:50:46 +0200

    ajout du fichier js pour le slider

---
+++ js/script_cam.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+
+var $carrousel = $('.carrousel'),
+	$page = $('.page'),
+	indexPage = $page.length - 1, //dernier index de la page 
+	i = 0,
+	$currentPage = $page.eq(i);
+
+$page.css('display', 'none');//cache les pages
+$currentPage.css('display', 'flex');// affiche la page courante
+
+
+
+$('.next').click(function(){
+	i++;
+
+	if (i <= indexPage){
+		$page.css('display', 'none');
+		$currentPage = $page.eq(i);
+		$currentPage.css('display', 'flex');
+	} else {
+		i = indexPage;
+	}
+});
+
+$('.prev').click(function(){
+	i--;
+
+	if (i >= 0) {				
+		$page.css('display', 'none');
+		$currentPage = $page.eq(i);
+		$currentPage.css('display', 'flex');
+	} else {
+		i = 0;
+	}
+});
+
+});
